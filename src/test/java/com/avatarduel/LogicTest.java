d9e45bbdff221308cfe4854b8c685aee508d3ad1
nafkhanzam <nafkhanalzamzami@gmail.com>
2020-04-08 17:23:32 +0700

    add junit test template

---
+++ src/test/java/com/avatarduel/LogicTest.java
@@ -0,0 +1,16 @@
+package com.avatarduel;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class LogicTest {
+
+    @DisplayName("Example Test Method with No Business Logic")
+    @Test
+    public void testTheTest() {
+        assertTrue(true);
+    }
+
+}
